5369c91fe1b21fbc207b13e5e322606744928748
Szymon Grzeszczuk <szymon3520@gmail.com>
2023-03-01 12:38:17 +0100

    initial commit

---
+++ front-end/RPG-session-assisstant/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <View className={'w-32 border-4'}>
+        <Text>asdfasdfasdfas</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

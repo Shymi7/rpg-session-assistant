5c82c6567f519cd19a3cad472f677148a8c54973
Michal <mbaka0002@gmail.com>
2023-03-09 08:48:38 +0100

    Backend start

---
+++ back-end/RPG-session-assisstant/src/main/java/com/zpsm/rpgsessionassisstant/RpgSessionAssisstantApplication.java
@@ -0,0 +1,13 @@
+package com.zpsm.rpgsessionassisstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSessionAssisstantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgSessionAssisstantApplication.class, args);
+    }
+
+}
---
+++ back-end/RPG-session-assisstant/src/test/java/com/zpsm/rpgsessionassisstant/RpgSessionAssisstantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpsm.rpgsessionassisstant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgSessionAssisstantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
